ff1f2775c47f2c82999d686e6d30bfeba2486562
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2024-04-02 23:29:37 +0700

    Create Home Page

---
+++ src/app/index.js
@@ -0,0 +1,27 @@
+import Code from "~/assets/icons/code";
+import Camera from "~/assets/icons/camera";
+import Ux from "~/assets/icons/ux";
+import Mentor from "~/assets/icons/mentor";
+
+export const descriptions = [
+  {
+    title: "Web Development",
+    text: "As a developer, I find myself most captivated by the power and flexibility of NEXT.js. I am always eager to dive into new projects that leverage NEXT.js and discover innovative ways to create fast, scalable, and user-friendly applications.",
+    icon: Code,
+  },
+  {
+    title: "App Development",
+    text: "With a focus on user-centric design and cutting-edge technologies, I thrive on building intuitive and efficient apps that make a positive impact on people's lives. Let's turn ideas into reality and shape the future together.",
+    icon: Camera,
+  },
+  {
+    title: "UI/UX Designing",
+    text: "Crafting visually appealing and intuitive user interfaces that offer a delightful user experience is something I'm truly fanatic about.",
+    icon: Ux,
+  },
+  {
+    title: "Mentorship",
+    text: "I have also found great joy in sharing my knowledge with others. Being a technical mentor allows me to give back to the community that has supported me throughout my career.",
+    icon: Mentor,
+  },
+];
