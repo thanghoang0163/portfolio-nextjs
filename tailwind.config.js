fcb8c5b3fea852681ed0885d7e3ff3d8158f6a86
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2024-04-02 23:27:35 +0700

    Config initial project

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        orange: "var(--orange)",
+        red: "var(--red)",
+        "dark-white": "var(--dark-white)",
+        "light-blue": "var(--light-blue)",
+        "light-orange": "var(--light-orange)",
+        "activate-white": "var(--activate-white)",
+        "blur-text-color": "var(--blur-text)",
+        "border-color": "var(--border)",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "primary-gradient":
+          "linear-gradient(91deg, #FF9C1A 7.92%, #E80505 108.2%)",
+      },
+      borderRadius: {
+        "primary-border-radius": "2rem",
+      },
+      boxShadow: {
+        "primary-shadow": "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
       },
     },
   },
